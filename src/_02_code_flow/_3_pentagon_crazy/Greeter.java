883e57395756a987c665fb12cd304019ff904d24
root <root@033c33cc5d34>
2020-10-17 01:31:07 +0000

    Working on greeter

---
+++ src/_02_code_flow/_3_pentagon_crazy/Greeter.java
@@ -0,0 +1,13 @@
+package _02_code_flow._3_pentagon_crazy;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	
+	String input = JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null, "Hello " + input + "!");
+	
+	
+}
+}
